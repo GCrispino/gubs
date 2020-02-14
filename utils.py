59921b4138085e94931b851b31d1d9f0b96f3bb6
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-14 18:57:51 -0300

    Add support for command line arguments

--- utils.py
+++ utils.py
@@ -8,15 +8,32 @@ def read_json(file_name):
 
 
 DEFAULT_FILE_INPUT = './env1.json'
+DEFAULT_KG = 0
+DEFAULT_LAMBDA = 0.1
+DEFAULT_OUTPUT = False
 
 
 def parse_args():
 
     parser = argparse.ArgumentParser(
-        description='LAO* algorithm implementation.')
+        description='GUBS (Goals with Utility-Based Semantic) algorithm implementation.')
 
     parser.add_argument('--file', dest='file_input',
                         default=DEFAULT_FILE_INPUT,
                         help="Environment JSON file used as input (default: %s)" % DEFAULT_FILE_INPUT)
-
+    parser.add_argument('--write_output', dest='output',
+                        default=DEFAULT_OUTPUT,
+                        action="store_true",
+                        help="Defines whether or not to write the algorithm output to a file (default: %s)" % DEFAULT_OUTPUT)
+    parser.add_argument('--c_max', dest='c_max', required=True,
+                        type=int,
+                        help="Maximum cost used in the algorithm")
+    parser.add_argument('--kg', dest='kg',
+                        default=DEFAULT_KG,
+                        type=float,
+                        help="Kg costant cost on goal states (default: %s)" % DEFAULT_KG)
+    parser.add_argument('--lambda', dest='lamb',
+                        default=DEFAULT_LAMBDA,
+                        type=float,
+                        help="Lambda risk factor (default: %s)" % DEFAULT_LAMBDA)
     return parser.parse_args()
